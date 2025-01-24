261465e832c0e8474f52769307471e8b1c70e727
Hevanafa <dhevansafa4@gmail.com>
2025-01-24 10:52:40 +0700

    added texture drawing example

--- src/game.rs
+++ src/game.rs
@@ -6,7 +6,7 @@ const WH: i32 = 360;
 pub fn main() {
 	let (mut rl, thread) = raylib::init().build();
 
-	print!("Hello");
+	print!("Starting raylib-rs...");
 
 	rl.set_window_size(WW, WH);
 
@@ -18,8 +18,6 @@ pub fn main() {
 	rl.set_window_title(&thread, "Raylib + Rust Starter");
 	rl.set_target_fps(60);
 
-	// Todo: draw the texture with alpha
-
 	let mut t: i32 = 0;
 
 	while !rl.window_should_close() {
@@ -28,8 +26,29 @@ pub fn main() {
 
 		// draw
 		let mut d = rl.begin_drawing(&thread);
+
 		d.clear_background(cornflower_blue);
 		d.draw_text("Hello Raylib + Rust!", 20, 20, 32, Color::RAYWHITE);
-		d.draw_texture(&raylib_logo, 20, 60, Color::RAYWHITE);
+		// d.draw_texture(&raylib_logo, 20, 60, Color::RAYWHITE);
+
+		{
+			let mut d = d.begin_blend_mode(BlendMode::BLEND_ALPHA);
+			let src_rec = Rectangle::new(
+				0.0, 0.0,
+				raylib_logo.width as f32, raylib_logo.height as f32);
+
+			let dest_rec = Rectangle::new(
+				WW as f32 / 2.0, WH as f32 / 2.0,
+				raylib_logo.width as f32, raylib_logo.height as f32);
+
+			d.draw_texture_pro(
+				&raylib_logo,
+				src_rec, dest_rec,
+				Vector2::new(
+					raylib_logo.width as f32 / 2.0,
+					raylib_logo.height as f32 / 2.0),
+				0.0,
+				Color::RAYWHITE.alpha((1.0 + (t / 30 % 2) as f32) / 2.0));
+		}
 	}
 }
